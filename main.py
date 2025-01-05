6ad2fc67d72a42e455f8bad59d0a0e5aecd1e666
Yordaign <Yordaign@protonmail.com>
2025-01-05 15:24:33 +0000

    Fixed FPS to 60, re-organised the main game loop to fix not working 'x' bug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import *
 def main():
     pygame.init(),
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    colour = (0,140,0)
+    colour = (0,0,0)
     fps = pygame.time.Clock()
     dt = 0
 
@@ -13,10 +13,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill(colour)
-            pygame.display.flip()
-            pygame.time.Clock().tick(60)
-            dt = fps / 1000
+        screen.fill(colour)
+        pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
