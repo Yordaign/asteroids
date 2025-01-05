e7a945f38ac9b723e02d84190c532749ee4a8ef5
Yordaign <Yordaign@protonmail.com>
2025-01-05 15:11:34 +0000

    readded constants.py, started work on fps

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ def main():
     pygame.init(),
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     colour = (0,140,0)
+    fps = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             screen.fill(colour)
             pygame.display.flip()
+            pygame.time.Clock().tick(60)
+            dt = fps / 1000
 
 if __name__ == "__main__":
     main()
