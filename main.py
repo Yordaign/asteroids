3c5cf367c22c96e383ff42b874456535d2a0242a
Yordaign <Yordaign@protonmail.com>
2025-01-06 00:19:15 +0000

    Added Player triangle to centre of screen, and plumbed in player class specifics

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init(),
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    colour = (0,0,0)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
+    background_colour = (0,0,0)
     fps = pygame.time.Clock()
     dt = 0
-
-
+  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(colour)
+        screen.fill(background_colour)
+        player.draw(screen)
         pygame.display.flip()
         dt = fps.tick(60) / 1000
 
