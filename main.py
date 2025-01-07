85eae4f5e1d12f8b44da1b36d94da028c2a4ecba
Yordaign <Yordaign@protonmail.com>
2025-01-07 23:14:54 +0000

    Added groups to de-clutter the gameloop

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from player import Player
 def main():
     pygame.init(),
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
@@ -14,16 +18,18 @@ def main():
     background_colour = (0,0,0)
     fps = pygame.time.Clock()
     dt = 0
-  
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(background_colour)
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = fps.tick(60) / 1000
-        player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
 
 if __name__ == "__main__":
     main()
